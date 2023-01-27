d58920e4135d516d9c7b46e38d553ca5e29d7aeb
JClark <mlitvinkox96@gmail.com>
2023-01-27 12:09:50 -0500

    Initial Setup

---
+++ src/main/java/com/github/johnclark96/voxelsniperaddongui/VoxelSniperAddonGui.java
@@ -0,0 +1,17 @@
+package com.github.johnclark96.voxelsniperaddongui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VoxelSniperAddonGui extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
